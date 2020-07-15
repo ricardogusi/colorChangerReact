de7b2395fd265e73f208401726bb84160c33c98f
ricardogusi <ricardogusi@hotmail.com>
2020-07-15 17:09:05 -0300

    refatoração da função HandleChange para usa-la apenas uma vez no código

--- src/component/selector.jsx
+++ src/component/selector.jsx
@@ -1,63 +1,51 @@
 import React, { useState } from "react";
 
 export default function Selector() {
-  let [color, setColor] = useState({
+  let [colors, setColor] = useState({
     red: 0,
     green: 0,
     blue: 0,
   });
 
-  let handleChangeRed = (e) => {
-    setColor(( color.red = { ...color, red:  e.target.value }));
-  };
-  let handleChangeGreen = (e) => {
-    setColor(( color.green = { ...color, green:  e.target.value }));
-  };
-  let handleChangeBlue = (e) => {
-    setColor(( color.blue = { ...color, blue:  e.target.value }));
-  };
+  const availableColors = Object.keys(colors)
 
+  let handleChange = (color) => {
+    return event => {
+      const {value} = event.target
+    
+    setColor({ ...colors, [color]:value });
+    }
+  }
+  
   return (
     <div className="selector">
       <h1>RGB COLOR CHANGER</h1>
       <div
         className="box"
         style={{
-          backgroundColor: `rgb(${color.red}, ${color.green}, ${color.blue})`,
+          backgroundColor: `rgb(${colors.red}, ${colors.green}, ${colors.blue})`,
         }}
       />
-      <h1>Selecione sua cor abaixo:</h1>R{" "}
+      <h1>Selecione sua cor abaixo:</h1>
+      {availableColors.map(color =>(
+      <div className="ranges">
+      <label className="label" >{color.toLocaleUpperCase()}</label>
       <input
+        
+        key={color}
         type="range"
-        name="red"
-        id="red"
+        name={color}
+        id={color}
         min="0"
         max="255"
-        value={color.red}
-        onChange={handleChangeRed}
+        value={color[color]}
+        onChange={handleChange(color)}
       />
-      <input type="text" name="valor" id="valorRed" value={color.red} />
-      <br />G{" "}
-      <input
-        type="range"
-        id="green"
-        min="0"
-        max="255"
-        value={color.green}
-        onChange={handleChangeGreen}
-      />
-      <input type="text" name="valor" id="valorGreen" value={color.green} />
-      <br />B{" "}
-      <input
-        type="range"
-        id="blue"
-        min="0"
-        max="255"
-        value={color.blue}
-        onChange={handleChangeBlue}
-      />
-      <input type="text" name="valor" id="valorBlue" value={color.blue} />
-      <br />
+      <br></br>
+      </div>
+      ))}
+      
+      
     </div>
   );
 }
